e59170f420d3edb997873264fd15e46846640d08
Jake Hyun <jake.hyun@hotmail.com>
2024-04-17 17:44:57 +0900

    Update vimrc and others

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 printf '%s\n' 'This will overwrite your vimrc.'
 printf '%s\n\n' 'Changes are as follows:'
 
-diff ~/.vimrc ./.vimrc --color
+diff ~/.vimrc ./vimrc --color
 
 printf '\n%s ' 'Press <Enter> to acknowledge and continue.'
 read _
-cp .vimrc ~/.vimrc
+cp ./vimrc ~/.vimrc
 
 mkdir ~/.vim -p
-cp .ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+cp ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 
 printf '%s\n\n' 'Operation complete!'
 printf '%s\n' 'Make sure to manually compile YCM if necessary.'
